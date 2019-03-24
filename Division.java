c07561edde8038f7582bb73acab4cad5570e5ad3
Chaitanya Sharma <chaitanya150895@gmail.com>
2019-03-24 20:23:01 +0530

    value changed for int, float and double

--- Division.java
+++ Division.java
@@ -3,19 +3,19 @@ class Division {
 public static void main(String args[]){
     int a,b,c;
     a = 5;
-    b = 3;
+    b =10;
     c = a / b;
     System.out.println("Result is " + c);
     
     float p,q,r;
     p = 4.9f;
-    q = 3.8f;
+    q = 13.8f;
     r = p / q;
     System.out.println("Result is " + r);
 
     double x, y, z;
     x = 4.9;
-    y = 3.8;
+    y = 33.8;
     z = x / y;
     System.out.println("Result is " + z);
 }
