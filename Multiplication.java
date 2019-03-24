6fccba9a06371b59e28988c1fa60c4a9291c1b32
Chaitanya Sharma <chaitanya150895@gmail.com>
2019-03-24 20:17:45 +0530

    Multiplication for int, float and double

---
+++ Multiplication.java
@@ -0,0 +1,22 @@
+class Multiplication {
+
+public static void main(String args[]){
+    int a,b,c;
+    a = 5;
+    b = 3;
+    c = a * b;
+    System.out.println("Result is " + c);
+    
+    float p,q,r;
+    p = 4.9f;
+    q = 3.8f;
+    r = p * q;
+    System.out.println("Result is " + r);
+
+    double x, y, z;
+    x = 4.9;
+    y = 3.8;
+    z = x * y;
+    System.out.println("Result is " + z);
+}
+}
