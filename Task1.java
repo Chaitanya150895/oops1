bbd9f8ec6ce4064f2ca99632457b7b26ca346364
Chaitanya Sharma <chaitanya150895@gmail.com>
2019-03-24 21:17:53 +0530

    Write a code to print the value of (a+b)^2. Where ^2 denotes power of 2.

---
+++ Task1.java
@@ -0,0 +1,13 @@
+//Write a code to print the value of (a+b)^2.where ^2 denotes power of 2
+
+class task1 {
+
+public static void main(String args[]){
+    
+    double a, b, c;
+    a = 4;
+    b = 3;
+    c =(a + b)*(a + b);
+    System.out.println("The value of (a+b)^2 is " + c);
+}
+}
