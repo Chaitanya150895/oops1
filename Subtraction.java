982a82d2fcd622e1050df8b327a756afd55aa20d
Chaitanya Sharma <chaitanya150895@gmail.com>
2019-03-24 10:15:36 +0530

    New class added for Subtraction Example

---
+++ Subtraction.java
@@ -0,0 +1,15 @@
+class Subtraction {
+
+public static void main(String args[]){
+    int a,b,c;
+    a = 5;
+    b = 3;
+    c = a - b;
+    System.out.println("Result is " + c);
+    
+}
+
+
+
+
+}
