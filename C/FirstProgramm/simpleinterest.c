72a68d794c28f3de98be5d8c1556f2586b50cf63
Chaitanya Sharma <chaitanya150895@gmail.com>
2019-03-28 22:11:21 +0530

    my first program in c

---
+++ C/FirstProgramm/simpleinterest.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+
+int main ()
+
+{
+    
+	int p, n;
+    
+	float r, si;
+
+    
+	p = 100;
+    
+	n = 2;
+    
+	r = 1.2;
+    
+	si = p*n*r/100;
+
+    
+	printf("%f\n", si);
+    
+	return 0;
+
+}
