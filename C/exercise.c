eb22fafbdd2cdec4252c08cfb571eb37875ddcc6
Chaitanya Sharma <chaitanya150895@gmail.com>
2019-04-01 20:54:57 +0530

    changed the limit of r=5 and c=4

--- C/exercise.c
+++ C/exercise.c
@@ -2,9 +2,9 @@
 int main()
 {
 	int r,c,sum;
-	for(r=1; r<=3; r++) /*outer loop*/
+	for(r=1; r<=5; r++) /*outer loop*/
 	{	
-		for (c=1; c<=2; c++) /*inner loop*/
+		for (c=1; c<=4; c++) /*inner loop*/
 		{
 			sum = r + c;
 			printf("r=%d c=%d sum =%d\n",r,c,sum);
