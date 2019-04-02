003db7334c11a264f07d8733395bb22ce4c47a33
Chaitanya Sharma <chaitanya150895@gmail.com>
2019-04-02 21:48:33 +0530

    to print all the possible combinations of 1 2 3

--- C/chapter5/Project5.c
+++ C/chapter5/Project5.c
@@ -1,16 +1,34 @@
 #include<stdio.h>
 int main ()
 {
-	int i, f;
+	
+	int i, f = 1, num;
+	
+	printf ("Enter a number");
+	scanf("%d",&num);
+
+	for (i = num; i>= 1; i--)
+	{
+		f = f * i;
+			
+	}
+	printf("%d", f);
+	
+	/*	
+	
+	int i, f = 1;
 	
 	printf ("Enter a number");
 	scanf("%d",&i);
 	
-	while (i <= 1)
+	
+	while (i >= 1)
 	{
-		f = i * i;
+		f = f * i;
 			
 		i = i - 1;
 	}
 	printf("%d", f);
+	
+	*/
 }
