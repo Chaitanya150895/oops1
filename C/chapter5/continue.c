99375622aedaac026bcd47aa38f623c335489920
sumit <sumit.tiwari@appxbuild.com>
2019-04-04 20:03:25 +0530

    implementing continue

---
+++ C/chapter5/continue.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+    int i, num;
+
+    for(i=1;i<=10;i++)
+    {
+        if (i==7)
+        continue;
+
+
+
+            printf("%d", i);
+            printf("\n");
+               
+    }
+    return 0;
+}
