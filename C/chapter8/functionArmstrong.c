d821ef84fd3d40aba783352647f6d4d045f1eeee
Chaitanya150895 <48850034+Chaitanya150895@users.noreply.github.com>
2019-04-05 18:12:43 +0530

    armstrong using function

---
+++ C/chapter8/functionArmstrong.c
@@ -0,0 +1,43 @@
+
+#include<stdio.h>
+
+void armStrong(int); 
+int main()
+
+{
+    int num;
+	armStrong(num);
+
+	return 0;
+}
+
+
+void armStrong(int a)
+
+{
+	int num, num1, num2, num3, num4;
+
+	for (num; num <= 500 ; num++ )
+	{
+
+		num1 = num % 10;
+		num2 = num1 % 10;
+		num3 = num2 % 10;
+
+		num4 = (num1 * num1 * num1) + (num2 * num2 * num2) + (num3 * num3 * num3);
+
+		if(num = num4)
+		
+		printf("%d is Armstrong number\n", num);
+
+		else 	
+		{
+
+		 printf("%d is not a Armstrong number\n", num);
+		 
+		 }
+
+	}
+
+}
+
