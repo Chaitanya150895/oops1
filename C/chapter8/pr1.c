f9b1095df8e56da6c982cb0d47ac60b27e15793e
sumit <sumit.tiwari@appxbuild.com>
2019-04-05 08:10:52 +0530

    second  function code

--- C/chapter8/pr1.c
+++ C/chapter8/pr1.c
@@ -1,23 +1,27 @@
 #include<stdio.h>
 //below is the important line to use function
 // we must declare the function name before main function
-void printTable(); 
+void printTable(int); 
 int main()
 {
-    printTable();
+    int num;
+    printf("Enter the value to print table");
+    scanf("%d",&num);
+
+    printTable(num);
 
     return 0;
 
 }
 
 //definition of a function is must to use it
-void printTable()
+void printTable(int a)
 {
-int i=0, num;
+int i=0;
 
-    num = 5;
+   
     for(i=0;i<11;i++)
     {
-        printf("%d x %d = %d\n",num, i, num * i);
+        printf("%d x %d = %d\n",a, i, a * i);
     }
 }
---
+++ C/chapter8/pr2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void italy();
+void brazil();
+void argentina();
+
+int main(){
+    
+    italy();
+    brazil();
+    argentina();
+
+
+    return 0;
+}
+
+void italy(){
+    printf("I am in Italy\n");
+}
+
+void brazil(){
+    printf("I am in Brazil\n");
+}
+
+void argentina(){
+    printf("I am in Argentina\n");
+}
