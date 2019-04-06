e2498bda92e32d53244683883d6a0a06d4ecc974
Chaitanya Sharma <chaitanya150895@gmail.com>
2019-04-06 11:08:58 +0530

    exercise 2

---
+++ C/chapter7/ex2.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main()
+
+{
+	int c ;
+	
+	switch (c)
+	{
+		case '3':
+		printf("char 3\n");
+		break;
+		
+		case 3:
+		printf("int 3\n");
+		break;
+		
+		default:
+		printf("Heart\n");
+	}
+	
+	return 0;
+}
+
+	
