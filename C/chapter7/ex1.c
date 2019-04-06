259bb3a3cc46d475c9f8c4207188e868860bd56a
Chaitanya Sharma <chaitanya150895@gmail.com>
2019-04-06 10:58:35 +0530

    exercise

---
+++ C/chapter7/ex1.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main()
+
+{
+	char suite = 2;
+	
+	
+	
+	switch (suite)
+	{
+		case 1:
+		printf("Diamond\n");
+		
+		case 2:
+		printf("Spade\n");
+		
+		default:
+		printf("Heart\n");
+	}
+	
+	printf("i thought one waers a suite\n");
+	return 0;
+}
+
+	
