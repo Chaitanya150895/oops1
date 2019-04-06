a265e11ebb58043a2242e74bb7fdca9e6bf7b014
Chaitanya Sharma <chaitanya150895@gmail.com>
2019-04-06 13:36:08 +0530

    exercise1

---
+++ C/chapter8/ex1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+void display();
+
+int main()
+{
+	printf("LearnC\n");
+	display();
+	return 0;
+	
+}
+
+void display()
+{
+	printf ("Followed by C++, C# and Java\n");
+	main ();
+	
+}
+
