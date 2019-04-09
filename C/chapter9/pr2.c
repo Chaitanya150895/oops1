4c6370f942f698e171d9f2fc5751d816aa4a7b8b
sumit <sumit.tiwari@appxbuild.com>
2019-04-09 08:31:47 +0530

    pointer project 1

--- C/chapter9/pr2.c
+++ C/chapter9/pr2.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 int main()
 {
-	int i = 5, *p, *q;
+	int i = 5, *p, **q;
 
 	p = &i;
 	
 	q = &p;
 	
-	printf("%d %d %u %u %d %u %u", i, *p, &i, p, q, &p, q);
+	printf("%d %d %u %u %u %u %u", i, *p, *(&i), p, q, &p, *q);
 	
 	//value of i assigning to p so i = p = 5
 	//address of p and q is same as i so 2293432
