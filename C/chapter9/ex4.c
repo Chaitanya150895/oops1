714b9ea315e5ee73408bc24264b10d3a9fea20b3
sumit <sumit.tiwari@appxbuild.com>
2019-04-10 08:43:43 +0530

    needd semicolon after the function declaration

---
+++ C/chapter9/ex4.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void jiaayjo (int , int); //needed semicolon
+int main ()
+{
+    int p =23 , f =24;
+    jiaayjo (&p, &f);
+    printf("%d%d\n", p, f);
+    return 0;
+}
+void jiaayjo (int q, int g)
+{
+        q = q + q;
+        g = g + g;
+}
