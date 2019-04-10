58db749b0e847d2ae07d68b686d6998936ae3c0b
sumit <sumit.tiwari@appxbuild.com>
2019-04-10 09:07:56 +0530

    err solved for exercise 7

---
+++ C/chapter9/ex7.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int function (int *); //return type should be int as returning the int value instead of void which was already mentioned the programm
+
+int main()
+{
+    int i = 35, *z;
+    z = function (&i);
+    printf("%d\n,z");
+    return 0;
+}
+
+int function (int *m)
+{
+    return (*m + 2);
+}
