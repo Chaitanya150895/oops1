2956b828e667f74a91366ef86de2cc04a6d76b27
sumit <sumit.tiwari@appxbuild.com>
2019-04-12 08:15:30 +0530

    using pointers

--- C/chapter9/ex8.c
+++ C/chapter9/ex8.c
@@ -6,22 +6,22 @@ the result in main().*/
 #include<stdio.h>
 #include <math.h>
 
-int mathfun(int a, int b, int c, int d, int e, int sum, int avg, int fun, int stddev)
+int mathfun(int *sum,int *avg, int *stddev)
 {
-	sum = a + b + c + d + e;
-	avg = sum / 5;
-	fun = ((sum - a)*(sum - a)+(sum - b)*(sum - b)+(sum - c)*(sum - c)+(sum - d)*(sum - d)+(sum - e)*(sum - e))/5;
-	stddev = sqrt(fun);
+	int a,b,c,d,e, fun;
+	printf("Enter the 5 integers\n");
+	scanf("%d%d%d%d%d", &a, &b, &c, &d, &e);
+	*sum = a + b + c + d + e;
+	*avg = *sum / 5;
+	fun = ((*sum - a)*(*sum - a)+(*sum - b)*(*sum - b)+(*sum - c)*(*sum - c)+(*sum - d)*(*sum - d)+(*sum - e)*(*sum - e))/5;
+	*stddev = sqrt(fun);
 	return 0;
 }
 
 int main()
 {
-	int a,b,c,d,e,sum,avg,stddev, fun;
-	printf("Enter the 5 integers\n");
-	scanf("%d%d%d%d%d", &a, &b, &c, &d, &e);
-	
-	mathfun(a,b,c,d,e,sum,avg,stddev, fun);
+	int sum, avg, stddev;
+	mathfun(&sum, &avg, &stddev);
 	
 	printf("\n\nSum is %d\nAverage is %d and \nStandard deviation is %d", sum, avg, stddev);
 
