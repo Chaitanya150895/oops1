83b72d781e04cacde38caf9e31d91c40e023319a
sumit <sumit.tiwari@appxbuild.com>
2019-04-12 08:24:00 +0530

    using pointers

--- C/chapter9/ex9.c
+++ C/chapter9/ex9.c
@@ -1,22 +1,22 @@
 #include<stdio.h>
 
-int student(int, int, int, int);
+int student(int *);
 
 int main()
 {
-	int s1, s2, s3, avg;
 	
-	printf("Enter the marks in 3 subjets");
-	scanf("%d%d%d", &s1, &s2, &s3);
-	
-	avg = student (s1, s2, s3, avg);
-		
+	int avg;
+	student (&avg);
+	printf("The average and percentage is %d", avg);
 	return 0;
 }
 
-int student(int s1, int s2, int s3, int avg)
+int student(int *avg)
 {
-	avg = (s1 + s2 + s3)/3;
-	printf("The average and percentage is %d", avg);
+	int s1, s2, s3;
+	printf("Enter the marks in 3 subjets");
+	scanf("%d%d%d", &s1, &s2, &s3);
+	*avg = (s1 + s2 + s3)/3;
+	
 	return 0;
 }
