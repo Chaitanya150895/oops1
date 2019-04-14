dd75f259e918213cb84baf5249c00382b40a4271
Chaitanya Sharma <chaitanya150895@gmail.com>
2019-04-14 11:04:51 +0530

    lines added

--- C/chapter9/ex13.c
+++ C/chapter9/ex13.c
@@ -27,8 +27,10 @@ int main()
 	a = sqrt((x2 - x1)*(x2 - x1))+((y2 - y1)*(y2 - y1));
 	b = sqrt((x3 - x2)*(x3 - x2))+((y3 - y2)*(y3 - y2));
 	c = sqrt((x1 - x3)*(x1 - x3))+((y1 - y3)*(y1 - y3));
+	
 	s=sideTriangle(&a,&b,&c);
 	area=areaTriangle(&s);
+	
 	printf("Distance between the two points A(x1, y1) and B(x2, y2) is : %f\n", a);
 	printf("Distance between the two points B(x2, y2) and C(x3, y3) is : %f\n", b);
 	printf("Distance between the two points C(x3, y3) and A(x1, y1) is : %f\n", c);
