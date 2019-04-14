ae4c0bbe8aa5195fd525afa66b2d785ae207e7ec
sumit <sumit.tiwari@appxbuild.com>
2019-04-14 14:05:00 +0530

    array programm

--- C/array/pr2.c
+++ C/array/pr2.c
@@ -5,13 +5,13 @@ int main()
     int num [4], i;
 
     printf("Enter num");      
-    for (i = 0; i <= 4; i++)
+    for (i = 0; i <= 7; i++)
     {
         scanf("%d", &num[i]);
     }
     
     num [i] = i;
-       for (i = 0; i <= 4; i++)
+       for (i = 0; i <= 7; i++)
     {
         printf("%d\n", num[i]);
     }
--- C/array/pr3.c
+++ C/array/pr3.c
@@ -3,5 +3,23 @@ void display1 (int);
 void display2 (int *);
 int main()
 {
-    int
+    int i;
+    int marks [] = {55,65,75,56,78,90};
+    printf("Display1: ");
+    for ( i = 0; i <6; i++)
+    display1 (marks[i]);
+    printf("\nDisplay2: ");
+    for (i = 0; i<6; i++)
+    display2 (&marks[i]);
+    return 0;
+}
+void display1 (int m)
+{
+    printf ("%d ", m);
+    
+}
+
+void display2(int *n)
+{   
+        printf("%d ", *n);
 }
