39fc93513f398d98111c625eac9981061d3f244a
sumit <sumit.tiwari@appxbuild.com>
2019-04-14 13:45:42 +0530

    enter num in array and print the same

---
+++ C/array/pr2.c
@@ -0,0 +1,20 @@
+//bounds checking
+#include<stdio.h>
+int main()
+{
+    int num [4], i;
+
+    printf("Enter num");      
+    for (i = 0; i <= 4; i++)
+    {
+        scanf("%d", &num[i]);
+    }
+    
+    num [i] = i;
+       for (i = 0; i <= 4; i++)
+    {
+        printf("%d\n", num[i]);
+    }
+
+    return 0;
+}
---
+++ C/array/pr3.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+void display1 (int);
+void display2 (int *);
+int main()
+{
+    int
+}
