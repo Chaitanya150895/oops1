be1cab6bd6564df1a9cfa7f51e977b800e48cb75
sumit <sumit.tiwari@appxbuild.com>
2019-04-15 08:47:22 +0530

    Flexible array size, correction done

--- C/array/pr9.c
+++ C/array/pr9.c
@@ -8,10 +8,10 @@ int main()
     scanf("%d",&max);
     p = (int *) malloc (max *sizeof (int));
 
-    for (i=0; i<=5; i++)
+    for (i=0; i<max; i++)
     {
         p[i] = i*i;
-        printf("%d",p[i]);
+        printf("%d,",p[i]);
     }
     return 0;
 }
