3a46a00d98ed5ceb5c4afeff02e5d4643e4268d8
Chaitanya150895 <48850034+Chaitanya150895@users.noreply.github.com>
2019-04-18 16:43:23 +0530

    pointer and string

---
+++ C/chapter15/pr7.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	char str1[] = "Hello";
+	char str2[10];
+	str *s = "Good Morning";
+	str *q;
+	str2 = str1;//err
+	q = s; //works
+	return 0;
+
+}
---
+++ C/chapter15/pr8.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	//char str1[] = "Hello";
+	char *p = "Hello";
+	//str1 = "Bye";
+	p = "Bye";
+	return 0;
+
+}
