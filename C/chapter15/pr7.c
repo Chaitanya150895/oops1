33dcc51cca759277f63351a7ab4a2dda205bb22a
sumit <sumit.tiwari@appxbuild.com>
2019-04-19 08:09:33 +0530

    one string can not assign to another but value of one pointer canm be assign to other

--- C/chapter15/pr7.c
+++ C/chapter15/pr7.c
@@ -3,9 +3,9 @@ int main()
 {
 	char str1[] = "Hello";
 	char str2[10];
-	str *s = "Good Morning";
-	str *q;
-	str2 = str1;//err
+	char *s = "Good Morning";
+	char *q;
+	//str2 = str1;//err
 	q = s; //works
 	return 0;
 
