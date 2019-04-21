3899d35d57f33f383e6f8e060b1c937a6e22ee00
sumit <sumit.tiwari@appxbuild.com>
2019-04-21 11:26:52 +0530

    string concatenation using standars libray function

--- C/chapter15/usingconst.c
+++ C/chapter15/usingconst.c
@@ -3,7 +3,7 @@ int main()
 {
     float r,a;
     const float pi = 3.14;
-    printf("Enter radius of the circle\n");
+    printf("Enter radius of the circle\n˜");
     scanf("%f", &r);
     a = pi *r*r;
     printf("Area of the circle = %f\n",a);
