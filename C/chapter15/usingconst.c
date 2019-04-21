fc5d0a1624fb85b36792fc96b7da5c832caf1e49
sumit <sumit.tiwari@appxbuild.com>
2019-04-21 11:13:24 +0530

    using keyword const in contaxt of ordinaty variables of type int, float

---
+++ C/chapter15/usingconst.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    float r,a;
+    const float pi = 3.14;
+    printf("Enter radius of the circle\n");
+    scanf("%f", &r);
+    a = pi *r*r;
+    printf("Area of the circle = %f\n",a);
+    return 0;
+}
