f13f1c9514a68ff338d2cfd72015d955c5bc805d
sumit <sumit.tiwari@appxbuild.com>
2019-04-22 08:16:20 +0530

    concatenation

--- C/chapter15/stringcat.c
+++ C/chapter15/stringcat.c
@@ -7,12 +7,16 @@ int main()
 {
 	char str1[30];
 	char str2[10];
+
 	printf("Enter the string\n");
 	scanf("%s", str1);
+
 	printf("Enter the string\n");
 	scanf("%s", str2);
+
 	strcat(str1, str2);
-	printf("After concatenation %s", &str1);
+
+	printf("After concatenation %s\n", str1);
 	return 0;
 }
 
@@ -20,9 +24,15 @@ void strcat(char* str1, char* str2)
 {
 	int i, j;
 	
-	for(i=0, j=0; str1[i]='\0', str2[j]='\0'; i++, j++)
+	for(i=0; str1[i] !='\0'; i++)
 	{
-		str1 = str2;	
+		;
+	}
+
+	for(j=0; str2[j] !='\0'; j++)
+	{
+		str1[i] = str2[j];	
+		i++;
 	}
 	
 }
