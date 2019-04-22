edbeb446ef6f4160b2ba4c87b5bf3598788e070f
sumit <sumit.tiwari@appxbuild.com>
2019-04-22 08:26:39 +0530

    Alice in Wonder Land

---
+++ C/chapter15/aliceinwonderland.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    char str1[10], str2[10], str3[10], str4[10];
+    printf("Enter the 4 strings\n");
+    scanf("%s%s%s%s", str1, str2, str3, str4);
+    printf("%s %s %s %s\n", str1, str2, str3, str4);
+    return 0;
+}
