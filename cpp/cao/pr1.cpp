ce644c7a604f961ed5254081a775484e3625eb92
sumit <sumit.tiwari@appxbuild.com>
2019-04-27 13:33:59 +0530

    class and object program 1

---
+++ cpp/cao/pr1.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class employee
+{
+    char name [80];  //private by default
+public:
+    void putname(char *n); //public
+    void getname(char *n);
+private:
+    double wage; //now , private again 
+public: 
+void putwage(double w); //back to public
+double getwage();
+};
+
+void employee::putname(char *n)
+{
+    strcpy(name, n);
+}
+
+void employee::getname(char *n)
+{
+    strcpy(n, name);
+}
+
+void employee::putwage(double w)
+{
+    wage = w;
+}
+double employee::getwage()
+{
+    return wage;
+}
+
+int main()
+{
+    employee ted;
+    char name[80];
+
+    ted.putname("Ted John");
+    ted.putwage(75000);
+
+    ted.getname(name);
+    cout << name << " makes $";
+    cout << ted.getwage() << " per year. ";
+
+    return 0;
+}
