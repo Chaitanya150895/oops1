e4ae157f9ca1c07f28ef53bb0174921cca50a95f
Chaitanya Sharma <chaitanya150895@gmail.com>
2019-04-28 12:04:26 +0530

    to define a to define an inline function within a class

---
+++ cpp/cao/pr11.cpp
@@ -0,0 +1,21 @@
+//Defining Inline function within a class
+
+#include<iostream>
+using namespace std;
+
+class myclass{
+	int a, b;
+public:
+	//automatic inline
+	void init(int i, int j) {a=i; b=j;}
+	void show() {cout<<a<<" "<<b<<"\n";}
+};
+
+int main()
+{
+	myclass x;
+	x.init(10, 20);
+	x.show();
+	
+	return 0;
+}
