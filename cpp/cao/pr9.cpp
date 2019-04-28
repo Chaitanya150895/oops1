23213bfd018a7141b28256394c750f67e3e4420b
Chaitanya Sharma <chaitanya150895@gmail.com>
2019-04-28 11:42:38 +0530

    inline function and equivalent

---
+++ cpp/cao/pr9.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+/*
+inline int max(int a, int b) //inline function
+{
+	return a>b ? a:b;
+}
+
+int main()
+{
+	cout<<max(10, 20);
+	cout<<" "<<max(99, 88);
+	
+	return 0;
+}
+*/
+
+
+int main()
+{
+	cout<<(10>20 ? 10:20);
+	cout<<" "<<(99>88 ? 99:88);
+	
+	return 0;
+}
