fe9c4d296f91dd14ace5695e4d092c0f0edf9d88
Chaitanya Sharma <chaitanya150895@gmail.com>
2019-04-28 12:17:56 +0530

    parametrized constructor

---
+++ cpp/cao/pr13.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+class myclass{
+	int a, b;
+public:
+	myclass(int i, int j) {a=i, b=j;}
+	void show() {cout<<a<<" "<<b;}	
+};
+
+int main()
+{
+	myclass ob(3, 5);
+	ob.show();
+	
+	return 0;
+}
