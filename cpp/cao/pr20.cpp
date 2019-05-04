f08de00aa6841bcf97c7a018e1fc8170aea9fe14
Chaitanya Sharma <chaitanya150895@gmail.com>
2019-05-04 14:02:01 +0530

    using parameterized constructor

---
+++ cpp/cao/pr20.cpp
@@ -0,0 +1,29 @@
+//using paremetrized constructor
+
+#include<iostream>
+class Point
+{
+	int x, y;
+	public:
+		Point (int a, int b) //inline parametrized constructor definition
+		{
+			x = a;
+			y = b;
+		}
+		void display()
+		{
+			cout<<"("<<x<<","<<y<<")\n";
+		}
+};
+
+int main()
+{
+	Point p1(1, 1); //invokes parametrized constructor
+	Point p2(5, 10);
+	cout<<"Point p1 = ";
+	p1.display();
+	cout<<"Point p2 = ";
+	p2.display();
+	
+	return 0;
+}
