2597c1ea84bca9fb68ab008e0087e082c0a8acd2
Chaitanya Sharma <chaitanya150895@gmail.com>
2019-05-04 22:06:06 +0530

    dynamic initialization of object err

---
+++ cpp/cao/pr22.cpp
@@ -0,0 +1,76 @@
+//long term fixed deposit system
+#include<iostream>
+using namespace std;
+
+class Fixed_deposit
+{
+	long int P_amount; //Principal amount
+	int Years; //Period of investment
+	float Rate; //Inerest rates
+	float R_value; //Return value of amount
+	
+	public:
+		Fixed_deposit(){}
+		Fixed_deposit (long int p, int y, float r = 0.12);
+		Fixed_deposit(long int p, int y, int r);
+		void display (void);
+};
+
+Fixed_deposit :: Fixed_deposit(long int p, int y, float r)
+{
+	P_amount = p;
+	Years = y;
+	Rate = r;
+	R_value = P_amount;
+	for(int i = 1; i<=y; i++)
+	R_value = R_value*(1.0+r);
+}
+
+Fixed_deposit :: Fixed_deposit(long int p, int y, int r)
+{
+	P_amount = p;
+	Years = y;
+	Rate = r;
+	R_value = P_amount;
+	for(int i = 1; i<=y; i++)
+	R_value = R_value*(1.0+float(r)/100);
+}
+
+void Fixed_deposit :: display(void)
+{
+	cout << "\n"
+		<<"Principal amount = " << P_amount << "\n"
+		<<"return value" = "<<R_value" << "\n";
+}
+
+int main()
+{
+	Fixed_deposit FD1, FD2, FD3; //deposits created
+	long int p; //principal amount
+	int y; //invested periods years
+	float r; //interest rates, decimal form
+	int R; //interest rate, percent form
+	
+	count << "Enter amount, period, interest rate(in percent)" <<"\n";
+	cin>>p>>y>>R;
+	FD1 = Fixed_deposit(p,y,R);
+	
+	cout << "Enter amount, period, interest rate(in decimal)" <<"\n";
+	cin>>p>>y>>r;
+	FD1 = Fixed_deposit(p,y,r);
+	
+	cout << "Enter amount and period" <<"\n";
+	cin>>p>>y;
+	FD1 = Fixed_deposit(p,y);
+	
+	cout << "\n Deposit 1";
+	FD1.display;
+	
+	cout << "\n Deposit 2";
+	FD2.display;
+	
+	cout << "\n Deposit 3";
+	FD3.display;
+	
+	return 0;
+}
