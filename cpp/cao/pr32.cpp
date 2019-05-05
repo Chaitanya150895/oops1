e3f934a84f7f3fed8e55ab33e34ff50f59ea9a4b
sumit <sumit.tiwari@appxbuild.com>
2019-05-05 12:07:27 +0530

    using this

--- cpp/cao/pr32.cpp
+++ cpp/cao/pr32.cpp
@@ -9,7 +9,7 @@ public:
     double get_pwr() {return val;}
 };
 
-
+/*
 pwr::pwr(double base, int exp)
 {
     b = base;
@@ -18,9 +18,9 @@ pwr::pwr(double base, int exp)
     if (exp == 0) return;
     for (; exp>0; exp --) val = val * b;
 }
+*/
 
 
-/*
 pwr::pwr (double base, int exp)
     {
         this ->b = base;
@@ -30,7 +30,7 @@ pwr::pwr (double base, int exp)
         for(; exp>0; exp--)
         this -> val = this ->val * this -> b; 
     }
-*/
+
 
 int main(){
     pwr x(4.0, 2), y(2.5, 1), z(5.7, 0);
