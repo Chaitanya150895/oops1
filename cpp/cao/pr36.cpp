8e883214504707618f277aeca170a9b1ab79d320
Chaitanya Sharma <chaitanya150895@gmail.com>
2019-05-05 22:30:46 +0530

    reference parameter

---
+++ cpp/cao/pr36.cpp
@@ -0,0 +1,23 @@
+//manually create a call by reference
+
+#include<iostream>
+using namespace std;
+
+void neg (int *i);
+
+int main()
+{
+	int x;
+	x = 10;
+	cout << x << "negated is";
+	
+	neg(&x);
+	cout << x << "\n"; 
+	
+	return 0;
+}
+
+void neg(int *i)
+{
+	*i = -*i;
+}
