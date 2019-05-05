91a381001367fcde087f2c5cc833024ced629231
sumit <sumit.tiwari@appxbuild.com>
2019-05-05 11:17:14 +0530

    Array of an object using constructor using longer initialize

---
+++ cpp/cao/pr28.cpp
@@ -0,0 +1,29 @@
+
+#include<iostream>
+using namespace std;
+class c1{
+    int h;
+    int i;
+    public:
+    c1(int j, int k) {h=j; i=k;} //constructor with two parameters
+    int get_i() {return i;}
+    int get_h() {return h;}
+};
+
+int main()
+{
+    c1 ob[3] = {
+        c1(1,2), //initialize
+        c1(3,4),
+        c1(5,6)
+    };
+
+    int i;
+
+    for (i=0; i<3; i++)
+        cout<<ob[i].get_h();
+        cout<< ", ";        
+        cout << ob[i].get_i() << "\n";
+
+        return 0;
+}
