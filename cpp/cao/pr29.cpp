e15ca1a1877078bed297e83823870a9aacc852f9
sumit <sumit.tiwari@appxbuild.com>
2019-05-05 11:30:42 +0530

    pointer to object

---
+++ cpp/cao/pr29.cpp
@@ -0,0 +1,20 @@
+//pointers to object
+#include<iostream>
+using namespace std;
+
+class c1{
+    int i;
+    public:
+    c1(int j) {i = j;}
+    int get_i() {return i;}
+};
+
+int main()
+
+{
+    c1 ob(88), *p;
+    p=&ob; //get address of ob
+    cout << p -> get_i(); //use -> to call get_i()
+    
+    return 0;
+}
