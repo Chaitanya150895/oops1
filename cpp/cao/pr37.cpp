911f24e2239e9d6a8f7ddace8982c48fd68f965f
Chaitanya Sharma <chaitanya150895@gmail.com>
2019-05-06 19:41:40 +0530

    neg using reference

---
+++ cpp/cao/pr37.cpp
@@ -0,0 +1,23 @@
+//use a reference parameter
+#include<iostream>
+using namespace std;
+
+void neg(int &i); //i now a reference
+
+int main()
+{
+	int x;
+	
+	x =10;
+	cout << x << "Negated is";
+	
+	neg(x); //no longer need the & operator
+	cout<<x<<"\n";
+	
+	return 0;
+}
+
+void neg(int &i)
+{
+	i = -i; //i is now a refence, don't need a reference
+}
