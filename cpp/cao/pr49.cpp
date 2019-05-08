cac24c06e9b546a71b79e250a2ce46efd7c71834
sumit <sumit.tiwari@appxbuild.com>
2019-05-08 20:41:46 +0530

    function overloading

---
+++ cpp/cao/pr49.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int myfunc(int i);      //these differ in types of parameters
+double myfunc(double i);
+
+
+int main()
+{
+cout << myfunc(10)<< " ";   //calls myfunc(int i)
+cout << myfunc(5.4);    //call myfunc(double i)
+
+return 0;
+
+}
+
+double myfunc(double i)
+{
+    return i;
+}
+
+int myfunc(int i)
+{
+    return i;
+}
+
+
+
+
+
+
