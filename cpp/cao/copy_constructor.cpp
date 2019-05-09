af5bf8bef7e37c134227f834a8f6d35c497f4157
sumit <sumit.tiwari@appxbuild.com>
2019-05-09 19:29:18 +0530

    basic copy constructor program

---
+++ cpp/cao/copy_constructor.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+class Samplecopyconstructor
+{
+    private:
+    int x, y; //data member
+    public:
+    Samplecopyconstructor(int x1, int y1)
+    {
+        x = x1;
+        y = y1;
+    }
+
+    /*Copy constructor*/
+    Samplecopyconstructor (const Samplecopyconstructor &Sam)
+    {
+        x = Sam.x;
+        y = Sam.y;
+    }
+
+    void display()
+    {
+        cout<< x << "  "<< y << endl;
+    }
+};
+
+/*main function*/
+int main()
+{
+    Samplecopyconstructor obj1(10, 15); //Normal constructor
+    Samplecopyconstructor obj2 = obj1; //Copy constructor
+
+    cout<< "Normal constructor: ";
+    obj1.display();
+    cout<< "Copy constructor: ";
+    obj2.display();
+
+    return 0;
+}
