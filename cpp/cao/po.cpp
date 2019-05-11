e0f6844924016a337d3a630f19cacde13d907a2d
sumit <sumit.tiwari@appxbuild.com>
2019-05-11 11:35:23 +0530

    did once without looking into book :D :D :D

---
+++ cpp/cao/po.cpp
@@ -0,0 +1,90 @@
+#include<iostream>
+using namespace std;
+
+class loc{
+    int longitude, latitude;
+public:
+    loc () {}
+
+    loc (int lg, int lt){
+        longitude = lg;
+        latitude = lt;
+    }
+
+    void updateValue (int lg, int lt){
+        longitude = lg;
+        latitude = lt;
+    }
+
+    void show(){
+    cout << longitude << " ";
+    cout << latitude << "\n"; 
+    }
+    
+    loc operator+(loc op2);
+    loc operator-(loc op2);
+    loc operator*(loc op2);
+    loc operator/(loc op2);
+
+};
+
+loc loc::operator+(loc op2){
+
+    loc temp;
+
+    temp.longitude = op2.longitude + longitude;
+    temp.latitude = op2.latitude + latitude;
+return temp;
+}
+
+loc loc::operator-(loc op2){
+
+    loc temp;
+
+    temp.longitude = op2.longitude - longitude;
+    temp.latitude = op2.latitude - latitude;
+return temp;
+}
+
+loc loc::operator*(loc op2){
+
+    loc temp;
+
+    temp.longitude = op2.longitude * longitude;
+    temp.latitude = op2.latitude * latitude;
+return temp;
+}
+
+loc loc::operator/(loc op2){
+
+    loc temp;
+
+    temp.longitude = op2.longitude / longitude;
+    temp.latitude = op2.latitude / latitude;
+
+    return temp;
+}
+
+int main(){
+    loc ob1(20,30), ob2(40,50);
+
+    ob1.show();
+    ob2.show();
+
+    ob1 = ob1 + ob2;
+    ob1.show();
+
+    ob1.updateValue(20,30);
+    ob1 = ob1 - ob2;
+    ob1.show();
+
+    ob1.updateValue (20, 30);
+    ob1 = ob1 * ob2;
+    ob1.show();
+
+    ob1.updateValue (20, 30);
+    ob1 = ob1 / ob2;
+    ob1.show();
+
+    return 0;
+}
