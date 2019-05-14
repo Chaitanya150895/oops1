42775bc5813ac6477a0f484a74e820d51de14f50
sumit <sumit.tiwari@appxbuild.com>
2019-05-14 19:55:25 +0530

    Inheritance and protected members

---
+++ cpp/cao/pr68.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+class base
+{
+    protected:
+    int i, j; //private to base and accissible by deerived
+    public:
+    void set(int a, int b)
+    {
+        i=a;
+        j=b;
+    }
+    void show() {
+        cout << i << " "<< j<< "\n";
+    }
+};
+
+class derived : public base{
+    int k;
+    public:
+    // derived may acees base's i and j
+    void setk() {k=i*j;}
+    void showk() {cout << k << "\n";}
+};
+
+int main()
+{
+    derived ob;
+
+    ob.set(2,3); //OK, known to derived
+    ob.show(); ///OK, known to derived
+
+    ob.setk();
+    ob.showk(); //uses member of derived class
+
+    return 0;
+}
